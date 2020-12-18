8ace4634348bbb9b64a749890a4c0d9d61442b58
Bruno Pinheiro <bpinheiroms@gmail.com>
2020-12-18 19:49:49 -0300

    implementacoes logos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import Head from 'next/head'
 
-import { Container, Header, Menus, Profile, Slider } from '../styles/pages/Home'
+import {
+  Container,
+  Header,
+  Menus,
+  Profile,
+  Slider,
+  MenusFixeds
+} from '../styles/pages/Home'
 import DisneyLogo from '../assets/icons/logo.svg'
 import MenusButton from '../components/menus-button.index'
 import ProfileImage from '../../public/woody_profile.jpg'
 import CarouselComponent from '../components/carousel.index'
+
+import DisneyCard from '../../public/fixeds/disney.png'
+import MarvelCard from '../../public/fixeds/marvel.png'
+import NationalCard from '../../public/fixeds/national.png'
+import PixarCard from '../../public/fixeds/pixar.png'
+import StarWarsCard from '../../public/fixeds/starwars.png'
+
 
 const Home: React.FC = () => {
   return (
@@ -25,6 +39,29 @@ const Home: React.FC = () => {
       <Slider>
         <CarouselComponent />
       </Slider>
+      <MenusFixeds>
+        <section className="categories-box">
+          <div className="categorie">
+            <img src={DisneyCard} alt="" />
+          </div>
+
+          <div className="categorie">
+            <img src={PixarCard} alt="" />
+          </div>
+
+          <div className="categorie">
+            <img src={MarvelCard} alt="" />
+          </div>
+
+          <div className="categorie">
+            <img src={StarWarsCard} alt="" />
+          </div>
+
+          <div className="categorie">
+            <img src={NationalCard} alt="" />
+          </div>
+        </section>
+      </MenusFixeds>
     </Container>
   )
 }
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -60,9 +60,6 @@ export const Profile = styled(Header)`
 export const Slider = styled.div`
   margin-top: 45px;
 
-  .slider-wrapper {
-    max-height: 312px !important;
-  }
   .card {
     position: relative;
   }
@@ -75,7 +72,7 @@ export const Slider = styled.div`
     transition-delay: 2s;
   }
 
-  li.slide{
+  li.slide {
     background: transparent !important;
   }
 
@@ -83,10 +80,44 @@ export const Slider = styled.div`
     padding-left: 20px !important;
   }
 
-  .card{
+  .card {
     width: 1240px;
-  object-fit: cover;
-  border-radius: 8px;
-  cursor: pointer;
+    object-fit: cover;
+    border-radius: 8px;
+    cursor: pointer;
+  }
+`
+export const MenusFixeds = styled.div`
+  margin-top: 32px;
+  width: 100%;
+
+  section {
+    padding: 0 80px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  section .categorie {
+    width: calc(20% - 20px);
+    background: linear-gradient(rgb(48, 50, 62), rgb(30, 31, 42)) 0% 0% / cover;
+
+    border-radius: 10px;
+    border: 2px solid #42444f;
+    box-shadow: rgba(0, 0, 0, 0.69) 0px 26px 30px -10px,
+      rgba(0, 0, 0, 0.73) 0px 16px 10px -10px;
+
+    cursor: pointer;
+  }
+
+  section .categorie:hover {
+    transform: scale(
+      1.1
+    ); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
+  }
+
+  section .categorie img {
+    width: 100%;
+    height: 100%;
   }
 `
